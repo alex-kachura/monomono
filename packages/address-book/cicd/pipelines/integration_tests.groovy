55cbfe7a3473b04c65a6bac47c1231606e5ed145
Vazzolla, Bogdan <Bogdan.Vazzolla@uk.tesco.com>
2019-07-10 13:25:32 +0100

    Merge pull request #42 from oneaccount/run-integration-tests-jenkins
    Run integration tests in Jenkins

---
+++ packages/address-book/cicd/pipelines/integration_tests.groovy
@@ -0,0 +1,70 @@
+#!/usr/bin/env groovy
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+def dateFormat = new SimpleDateFormat("yyyyMMddHHmm")
+def timestamp = dateFormat.format(new Date())
+def label = "address-book-builder-${UUID.randomUUID().toString()}"
+def branch
+def jobName = "${env.JOB_NAME}-${env.BUILD_NUMBER}"
+def commitSha
+
+try {
+	podTemplate(
+		label: label,
+		serviceAccount: 'jenkins',
+		containers: [
+			containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:alpine', resourceRequestCpu: '500m', resourceLimitCpu: '500m', resourceRequestMemory: '300Mi', resourceLimitMemory: '300Mi', envVars: [envVar(key: 'JAVA_OPTS', value: '-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XshowSettings:vm')]),
+			containerTemplate(name: 'docker', image: 'docker:18.02', command: 'cat', ttyEnabled: true,resourceRequestCpu: '500m', resourceLimitCpu: '500m', resourceRequestMemory: '300Mi', resourceLimitMemory: '300Mi'),
+			containerTemplate(name: 'awscli', image: 'quay.io/coreos/awscli:master', command: 'cat', ttyEnabled: true,resourceRequestCpu: '500m', resourceLimitCpu: '500m', resourceRequestMemory: '300Mi', resourceLimitMemory: '300Mi'),
+			containerTemplate(name: 'hyperaurora', image:  '914904879356.dkr.ecr.eu-west-1.amazonaws.com/hyperaurora:latest', command: 'cat', alwaysPullImage: true, ttyEnabled: true,resourceRequestCpu: '500m', resourceLimitCpu: '500m', resourceRequestMemory: '300Mi', resourceLimitMemory: '300Mi')
+		],
+		volumes: [
+			hostPathVolume(hostPath: '/var/run/secure/jenkins-docker.sock', mountPath: '/var/run/docker.sock'),
+			secretVolume(secretName: 'tiller-secret', mountPath: '/tiller')
+		],
+		annotations: [
+			podAnnotation(key: "iam.amazonaws.com/role", value: "jenkins-slave-oneaccount")
+		]) {
+
+		node(label) {
+			stage('Checkout the repository') {
+        def gitData = checkout(scm)
+				commitSha = gitData.GIT_COMMIT.trim()
+        branch = gitData.GIT_BRANCH.tokenize('/')[-1]
+			}
+
+			stage('Login into AWS ECR') {
+				container('awscli') {
+					sh 'aws ecr get-login --region eu-west-1 --no-include-email > ecr-login.sh'
+					sh 'chmod +x ecr-login.sh'
+				}
+				container('docker') {
+					sh "./ecr-login.sh"
+				}
+			}
+
+			stage('Run integration tests') {
+				container('docker') {
+				  sh "cd test/integration"
+				  sh "npm run integration:ppe"
+				}
+			}
+		}
+	}
+
+  notifyOnSlack("good", "[<$BUILD_URL|$JOB_NAME #$BUILD_NUMBER>]: 🎉 Integration tests passed! 🎉")
+	currentBuild.result = 'SUCCESS'
+} catch (e) {
+  notifyOnSlack("danger", "[<$BUILD_URL|$JOB_NAME #$BUILD_NUMBER>]: 🔥 Job failed! 🔥\n${e.getMessage()}")
+	println('Failure' + e.getMessage())
+	currentBuild.result = 'FAILURE'
+}
+
+def notifyOnSlack(toneColor, message) {
+  slackSend channel: '#oneaccount',
+            teamDomain: 'tescooneaccount',
+            color: toneColor,
+            message: message
+}
