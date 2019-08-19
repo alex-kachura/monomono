f7586a55cca50ce8694adef405ccebac8cfb7197
Vazzolla, Bogdan <Bogdan.Vazzolla@uk.tesco.com>
2019-08-19 14:39:24 +0100

    Merge pull request #68 from oneaccount/mtm-5046-int-test
    MTM-5046: Inline mode integration test

--- packages/address-book/test/integration/specs/landing-page.test.js
+++ packages/address-book/test/integration/specs/landing-page.test.js
@@ -14,16 +14,76 @@ let $;
 describe.each(contexts())('Landing Page (/)', (context) => {
   const { appUrl, region } = context;
 
-  describe(region, () => {
-    beforeAll(async () => {
-      response = await getResponse(`${appUrl}/`, { jar, headers });
+  describe('Non inline mode', () => {
+    describe(region, () => {
+      beforeAll(async () => {
+        response = await getResponse(`${appUrl}/`, { jar, headers });
 
+        $ = cheerio.load(response.body);
+      });
+
+      it('should load successfully', () => {
+        expect(response.statusCode).toBe(200);
+        expect(response.request.href).toBe(`${appUrl}/`);
+      });
+
+      it('should have a header', () => {
+        expect($('header').length).toBe(1);
+      });
+
+      it('should have a footer', () => {
+        expect($('footer').length).toBe(1);
+      });
+
+      it('should have a breadcrumb', () => {
+        expect($('nav[aria-label=breadcrumb]').length).toBe(1);
+      });
+
+      it('should contain correct page title', () => {
+        const pageTitle = context.getLocalePhrase(context.lang, 'pages.landing.title');
+
+        expect($('h1').text()).toContain(pageTitle);
+      });
+
+      it('should contain Clubcard address', () => {
+        const clubcardAddressTitle = context.getLocalePhrase(
+          context.lang,
+          'pages.landing.primary-address.header.clubcard',
+        );
+
+        expect($('h5').text()).toContain(clubcardAddressTitle);
+      });
+
+      it('should contain Delivery Address', () => {
+        const groceryAddressTitle = context.getLocalePhrase(
+          context.lang,
+          'pages.landing.primary-address.header.grocery',
+        );
+
+        expect($('h5').text()).toContain(groceryAddressTitle);
+      });
+    });
+  });
+  describe('Inline mode', () => {
+    beforeAll(async () => {
+      response = await getResponse(`${appUrl}?consumer='test'`, { jar, headers });
       $ = cheerio.load(response.body);
     });
 
     it('should load successfully', () => {
       expect(response.statusCode).toBe(200);
-      expect(response.request.href).toBe(`${appUrl}/`);
+    });
+
+    it('should not have a header', () => {
+      expect($('header').length).toBe(0);
+    });
+
+    it('should not have a footer', () => {
+      expect($('footer').length).toBe(0);
+    });
+
+    it('should not have a breadcrumb', () => {
+      expect($('nav[aria-label=breadcrumb]').length).toBe(0);
     });
 
     it('should contain correct page title', () => {
